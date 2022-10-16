403445d83322b29f7871668a5be2ba3edb3a0b17
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-10-16 22:43:16 +0700

    darktheme

--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -1,16 +1,62 @@
 import classNames from 'classnames/bind';
 import Button from '~/components/Button';
 import styles from './Menu.module.scss';
+import clsx from 'clsx';
+import { ChangeEventHandler } from "react";
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ data, onClick }) {
+const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+};
+
+const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+};
+
+const storedTheme = localStorage.getItem("theme");
+
+const prefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const defaultDark =
+    storedTheme === "dark" || (storedTheme === null && prefersDark);
+
+if (defaultDark) {
+    setDark();
+}
+
+const toggleTheme = (e) => {
+    if (e.target.checked) {
+        setDark();
+    } else {
+        setLight();
+    }
+};
+
+function MenuItem({ data, onClick, onThemeChange = false }) {
     const classes = cx('menu-item', {
         separate: data.separate,
     });
     return (
         <Button className={classes} leftIcon={data.icon} to={data.to} onClick={onClick}>
             {data.title}
+            {onThemeChange &&
+                <div className={cx('toggle-theme-wrapper')}>
+                    <label className={cx('toggle-theme')} htmlFor="checkbox">
+                        <input
+                            type="checkbox"
+                            id="checkbox"
+                            onChange={toggleTheme}
+                            defaultChecked={defaultDark}
+                        />
+                        <div className={clsx(cx('slider'), cx('round'))}></div>
+                    </label>
+                </div>
+            }
         </Button>
     );
 }
